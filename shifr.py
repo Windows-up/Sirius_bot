2a0056c8df2a1a1d8a3cd10adcc3fe7481604fc2
Doctor <jebpip2008@gmail.com>
2021-12-19 21:21:59 +0300

    Initial commit

---
+++ shifr.py
@@ -0,0 +1,34 @@
+from config import *
+
+def toSecure(inp):
+    result = ""
+
+
+    for i in range(0,len(inp)):
+        if inp[i] == '\n' :
+            result += "`"
+        elif inp[i] not in alfabet:
+            pass
+        else:
+            index = int(alfabet.index(inp[i])) + add
+            if index >= len(alfabet):
+                index -= len(alfabet)
+            result += alfabet[index]
+
+    return result
+
+def deSecure(inp):
+    result = ""
+
+    for i in range(0, len(inp)):
+        if inp[i] == "`":
+            result += "\n"
+        elif inp[i] not in alfabet:
+            result += ""
+        else:
+            index = alfabet.index(inp[i]) - add
+            if index < 0:
+                index = len(alfabet) + index
+            result += alfabet[index]
+
+    return result
